44bba9ef04205edf837f13bc6395cf4d2a3a2db3
Luca <luc.fabbian@gmail.com>
2018-10-28 22:45:06 +0100

    Switched to lambda functions

--- da-vinci-app-onsen/js/davapi.js
+++ da-vinci-app-onsen/js/davapi.js
@@ -1,42 +1,22 @@
-app.davinciApi = {};
+app.davinciApi = {
+  
+  // Base url
+  apiUrl: "http://www.liceodavinci.tv/api/",
+  
+  // Check if api is online
+  isOnline: () => axios.get(app.davinciApi.apiUrl+"teapot")
+    .catch(function (error) { return error.response.status === 418; }),
 
-app.davinciApi.apiUrl = "http://www.liceodavinci.tv/api/";
+  // Generic requests
+  getAgenda: (filter) => axios.post(app.davinciApi.apiUrl + "agenda", filter),
+  getClassi: ()       => axios.get(app.davinciApi.apiUrl + "classi"),
+  getOrarioClasse: (classe) => axios.get(app.davinciApi.apiUrl+"orario/"+classe),
+  getDocenti: () => axios.get(app.davinciApi.apiUrl+"docenti"),
+  getOrarioDocente: (docente) => axios.post(app.davinciApi.apiUrl+"orario/docente", docente),
+  
+  // Comunicati
+  getComunicatiStudenti: (last_n) => axios.get(app.davinciApi.apiUrl+"comunicati/studenti/"+last_n),
+  getComunicatiGenitori: (last_n) => axios.get(app.davinciApi.apiUrl+"comunicati/genitori/"+last_n),
+  getComunicatiDocenti : (last_n) => axios.get(app.davinciApi.apiUrl+"comunicati/docenti/"+last_n),
 
-app.davinciApi.isOnline = function () {
-    return axios.get(app.davinciApi.apiUrl+"teapot")
-        .catch(function (error) {
-            return error.response.status == 418;
-        });
-}
-
-app.davinciApi.getAgenda = function (filter) {
-    return axios.post(app.davinciApi.apiUrl+"agenda", filter);
-}
-
-app.davinciApi.getClassi = function () {
-    return axios.get(app.davinciApi.apiUrl+"classi");
-}
-
-app.davinciApi.getComunicatiStudenti = function (last_n) {
-    return axios.get(app.davinciApi.apiUrl+"comunicati/studenti/"+last_n);
-}
-
-app.davinciApi.getComunicatiGenitori = function (last_n) {
-    return axios.get(app.davinciApi.apiUrl+"comunicati/genitori/"+last_n);
-}
-
-app.davinciApi.getComunicatiDocenti = function (last_n) {
-    return axios.get(app.davinciApi.apiUrl+"comunicati/docenti/"+last_n);
-}
-
-app.davinciApi.getDocenti = function () {
-    return axios.get(app.davinciApi.apiUrl+"docenti");
-}
-
-app.davinciApi.getOrarioClasse = function (classe) {
-    return axios.get(app.davinciApi.apiUrl+"orario/"+classe);
-}
-
-app.davinciApi.getOrarioDocente = function (docente) {
-    return axios.post(app.davinciApi.apiUrl+"orario/docente", docente);
 }
