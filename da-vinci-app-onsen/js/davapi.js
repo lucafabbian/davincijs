3b508c1647df90dc85bec8a16926e2bf209af8d3
Antonio Napolitano <nap@antonionapolitano.eu>
2018-10-28 22:31:02 +0100

    integrazione funzioni api in app.davinciApi

--- da-vinci-app-onsen/js/davapi.js
+++ da-vinci-app-onsen/js/davapi.js
@@ -1,40 +1,42 @@
-var apiUrl = "http://www.liceodavinci.tv/api/";
+app.davinciApi = {};
 
-function isOnline() {
-    return axios.get(apiUrl+"teapot")
+app.davinciApi.apiUrl = "http://www.liceodavinci.tv/api/";
+
+app.davinciApi.isOnline = function () {
+    return axios.get(app.davinciApi.apiUrl+"teapot")
         .catch(function (error) {
             return error.response.status == 418;
         });
 }
 
-function getAgenda(filter) {
-    return axios.post(apiUrl+"agenda", filter);
+app.davinciApi.getAgenda = function (filter) {
+    return axios.post(app.davinciApi.apiUrl+"agenda", filter);
 }
 
-function getClassi() {
-    return axios.get(apiUrl+"classi");
+app.davinciApi.getClassi = function () {
+    return axios.get(app.davinciApi.apiUrl+"classi");
 }
 
-function getComunicatiStudenti(last_n) {
-    return axios.get(apiUrl+"comunicati/studenti/"+last_n);
+app.davinciApi.getComunicatiStudenti = function (last_n) {
+    return axios.get(app.davinciApi.apiUrl+"comunicati/studenti/"+last_n);
 }
 
-function getComunicatiGenitori(last_n) {
-    return axios.get(apiUrl+"comunicati/genitori/"+last_n);
+app.davinciApi.getComunicatiGenitori = function (last_n) {
+    return axios.get(app.davinciApi.apiUrl+"comunicati/genitori/"+last_n);
 }
 
-function getComunicatiDocenti(last_n) {
-    return axios.get(apiUrl+"comunicati/docenti/"+last_n);
+app.davinciApi.getComunicatiDocenti = function (last_n) {
+    return axios.get(app.davinciApi.apiUrl+"comunicati/docenti/"+last_n);
 }
 
-function getDocenti() {
-    return axios.get(apiUrl+"docenti");
+app.davinciApi.getDocenti = function () {
+    return axios.get(app.davinciApi.apiUrl+"docenti");
 }
 
-function getOrarioClasse(classe) {
-    return axios.get(apiUrl+"orario/"+classe);
+app.davinciApi.getOrarioClasse = function (classe) {
+    return axios.get(app.davinciApi.apiUrl+"orario/"+classe);
 }
 
-function getOrarioDocente(docente) {
-    return axios.post(apiUrl+"orario/docente", docente);
+app.davinciApi.getOrarioDocente = function (docente) {
+    return axios.post(app.davinciApi.apiUrl+"orario/docente", docente);
 }
--- da-vinci-app-onsen/js/index.js
+++ da-vinci-app-onsen/js/index.js
@@ -6,3 +6,7 @@ var vm = new Vue({
   data: app.data
  })
 console.log("Vue")
+app.davinciApi.isOnline()
+  .then(function (result) {
+    console.log("Api online: "+result);
+  });
