0fe5dc06d94b3e2dcc928f1c5499b9d8d43a7dc1
Antonio Napolitano <nap@antonionapolitano.eu>
2018-10-29 12:31:04 +0100

    fix cross origin pdf.js

--- da-vinci-app/res/pdfjs/web/viewer.js
+++ da-vinci-app/res/pdfjs/web/viewer.js
@@ -1595,7 +1595,7 @@ function webViewerInitialized() {
   var queryString = document.location.search.substring(1);
   var params = (0, _ui_utils.parseQueryString)(queryString);
   file = 'file' in params ? params.file : _app_options.AppOptions.get('defaultUrl');
-  validateFileURL(file);
+  //validateFileURL(file);
   var fileInput = document.createElement('input');
   fileInput.id = appConfig.openFileInputName;
   fileInput.className = 'fileInput';
