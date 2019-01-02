7529a5d72c9c2a98a507c75d6c33d9c3828bc29f
Luca <luc.fabbian@gmail.com>
2019-01-02 22:31:13 +0100

    Moved menu data from main to app-menu-data.js

--- da-vinci-app/js/components/layout/app-menu.js
+++ da-vinci-app/js/components/layout/app-menu.js
@@ -1,14 +1,16 @@
+import AppMenuData from './app-menu-data.js'
+console.log(AppMenuData)
 export default {
   template: `<ons-page>
     <div class="profile-pic">
-      <img :src="$root.menu.image" style="display: block; max-width: 100%;">
+      <img :src="image" style="display: block; max-width: 100%;">
     </div>      
     <ons-list>
-      <span v-for="category in $root.menu.categories">
+      <span v-for="category in categories">
         <span v-if=" category.name != ''">
           <br><ons-list-title>{{ category.name }}</ons-list-title>
         </span>
-        <ons-list-item v-for="element in category.elements" v-on:click="changePage(element.page)" tappable>
+        <ons-list-item v-for="element in category.elements" v-on:click="changePage(element)" tappable>
           <div class="left">
             <ons-icon fixed-width class="list-item__icon" :icon="element.icon"></ons-icon>
           </div>
@@ -17,6 +19,9 @@ export default {
       </span>
     </ons-list>
   </ons-page>`,
+  data: function(){
+    return AppMenuData
+  },
   methods: {
     changePage: function (page) {
       this.$root.page = page
--- da-vinci-app/js/index.js
+++ da-vinci-app/js/index.js
@@ -7,39 +7,15 @@ Vue.use(AppComponents)
 new Vue({  
   el: '#app', 
   data: {
-    page: (localStorage.page || 'app-page-home'),
+    page: (localStorage.page || {name: "Home",   icon:"md-home",     page:"app-page-home"} ),
     comunicatiLetti     : JSON.parse(localStorage.comunicatiLetti     || '[]'),
     comunicatiPreferiti : JSON.parse(localStorage.comunicatiPreferiti || '[]'),
-    menu: {
-      image: 'res/img/logo-toolbar.svg', 
-      categories: [ {
-          name: "",
-          elements:[
-            {name: "Home",   icon:"md-home",     page:"app-page-home"},    
-            {name: "Agenda", icon:"md-calendar", page:"app-page-agenda"},    
-            {name: "Orari",  icon:"md-time"},      
-          ]
-        }, {
-          name: "comunicati",
-          elements:[
-            {name: "Studenti",  icon:"md-graduation-cap", page:"app-page-comunicati-studenti"},      
-            {name: "Genitori",  icon:"md-accounts",       page:"app-page-comunicati-genitori"},      
-            {name: "Docenti",   icon:"md-case",           page:"app-page-comunicati-docenti"}, 
-            {name: "Preferiti",   icon:"md-star",         page:"app-page-comunicati-preferiti"},                 
-          ]
-        }, {
-          name: "utilità",
-          elements:[
-            {name: "Impostazioni", icon:"md-settings", page:"app-page-impostazioni"}
-          ]
-        }, ],
-    }
     
   },
   watch: {
     comunicatiLetti    () { localStorage.comunicatiLetti     = JSON.stringify(this.comunicatiLetti    )  },
     comunicatiPreferiti() { localStorage.comunicatiPreferiti = JSON.stringify(this.comunicatiPreferiti)  },
-    page               () { localStorage.page                = this.page                          },
+    page               () { localStorage.page                = JSON.stringify(this.page               )  },
 
   },
 })
